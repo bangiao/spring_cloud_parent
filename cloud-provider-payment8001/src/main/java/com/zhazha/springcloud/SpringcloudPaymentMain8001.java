4a1d3c21c5c53edfe71648f258a4027ff15c206a
2133445917@qq.com <2133445917@qq.com>
2022-09-02 20:45:48 +0800

    rest basic

---
+++ cloud-provider-payment8001/src/main/java/com/zhazha/springcloud/SpringcloudPaymentMain8001.java
@@ -0,0 +1,13 @@
+package com.zhazha.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudPaymentMain8001 {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringcloudPaymentMain8001.class, args);
+	}
+	
+}
