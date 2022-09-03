147ebc3c758dd2d959857bd855dc5d5d1959ee30
2133445917@qq.com <2133445917@qq.com>
2022-09-03 21:23:38 +0800

    consumer order 项目

--- cloud-consumer-order80/src/main/java/com/zhazha/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/zhazha/springcloud/OrderMain80.java
@@ -3,11 +3,12 @@ package com.zhazha.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringcloudPaymentMain8001 {
+public class OrderMain80 {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringcloudPaymentMain8001.class, args);
+		SpringApplication.run(OrderMain80.class, args);
 	}
 	
 }
--- cloud-provider-payment8001/src/main/java/com/zhazha/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zhazha/springcloud/PaymentMain8001.java
@@ -3,12 +3,11 @@ package com.zhazha.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class ConsumerOrderMain80 {
+public class PaymentMain8001 {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(ConsumerOrderMain80.class, args);
+		SpringApplication.run(PaymentMain8001.class, args);
 	}
 	
 }
