0090959f0318937a46a80343c12c0cc3a7eff8ff
2133445917@qq.com <2133445917@qq.com>
2022-09-13 17:51:23 +0800

    gateway添加GatewayConfig

---
+++ cloud-gateway-gateway9527/src/main/java/com/zhazha/springcloud/config/GateWayConfig.java
@@ -0,0 +1,31 @@
+package com.zhazha.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+
+        routes.route("path_route_zhazha",
+                r -> r.path("/guonei")
+                        .uri("https://news.baidu.com")).build();
+
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_zhazha2",
+                r -> r.path("/guoji")
+                        .uri("https://news.baidu.com")).build();
+        return routes.build();
+    }
+
+}
