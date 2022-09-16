259e2fbe124bf9c728eb6a1a658b0ac7365b8339
2133445917@qq.com <2133445917@qq.com>
2022-09-16 21:02:32 +0800

    gateway代码提交

---
+++ cloud-gateway-gateway9527/src/main/java/com/zhazha/springcloud/predicates/TokenRoutePredicateFactory.java
@@ -0,0 +1,34 @@
+package com.zhazha.springcloud.predicates;
+
+import com.zhazha.springcloud.predicates.config.MyConfig;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class TokenRoutePredicateFactory extends AbstractRoutePredicateFactory<MyConfig> {
+
+    public TokenRoutePredicateFactory() {
+        super(MyConfig.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("token");
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(MyConfig config) {
+        return serverWebExchange -> {
+            // 拿到网页参数给出的 token 值
+            String token = serverWebExchange.getRequest().getQueryParams().getFirst("token");
+            // config.getToken() 找到application.yml配置的值 - Token=myToken123456
+            // 两个值进行对比, 如果相同 返回 true, 否则返回 false
+            return StringUtils.equalsAnyIgnoreCase(token, config.getToken());
+        };
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/zhazha/springcloud/predicates/config/MyConfig.java
@@ -0,0 +1,13 @@
+package com.zhazha.springcloud.predicates.config;
+
+public class MyConfig {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
