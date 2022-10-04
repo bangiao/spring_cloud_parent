bba555330fff1cce00796583032bb643822eb441
2133445917@qq.com <2133445917@qq.com>
2022-10-04 21:06:56 +0800

    webflux简单入门

---
+++ webflux-demo/src/main/java/com/zhazha/WebfluxApplication.java
@@ -0,0 +1,14 @@
+package com.zhazha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WebfluxApplication.class, args);
+	}
+
+}
